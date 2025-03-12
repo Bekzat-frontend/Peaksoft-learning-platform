53dac12dd27d8e77f76c73e9c513ef12c52fbac9
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-13 05:53:22 +0700

    strains

--- src/coursePage/Material.jsx
+++ src/coursePage/Material.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Pagination from "./Pagination";
 
 function Material() {
   return (
@@ -22,6 +23,7 @@ function Material() {
           6 Lesson - 6 State Batch | Lifting state up | Children
         </StyledLesson>
       </StyledDivHome>
+      <Pagination />
     </StyledDiv>
   );
 }
